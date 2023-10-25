9ff310cf6e8bd5d9629a2af9582501743045c99b
Jakub Sosnowski <jakub.sosnowski@op.pl>
2023-10-25 20:33:08 +0200

    Poprawka w komentarzu

--- guess_number.py
+++ guess_number.py
@@ -10,7 +10,7 @@ while True:
         continue
     if user_choose > 100 or user_choose < 0:
         print("Please enter value with range 0-100")
-        # I added this waring because user doesn't know range of number
+        # I added this warning because user doesn't know range of number
     elif user_choose < comp_choose:
         print("Too small!")
     elif user_choose > comp_choose:
